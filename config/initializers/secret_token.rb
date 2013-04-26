9dbabb54aaec1326a60c229167cbc36bbbdde703
vipul <vipuls12@yahoo.com>
2013-04-26 23:57:45 +0530

    fst commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fst::Application.config.secret_token = '986580d6ede3b4cc6c111e3ad156ab645ec4ab411e6d5856302529f12a2af2f8f35afcc607d7b0c7e57befd3603d04bd9dfbf147b8ca1d410255e32d11c793d9'
