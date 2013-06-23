83fe6e5ddc560fb762158dd204d15e8514580ca6
vikrantmetawing <vikrantmetawing@gmail.com>
2013-06-23 10:02:15 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,9 @@ Fst::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-
+  
+  config.assets.precompile += ['960sm.css']
+  
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -15,7 +17,7 @@ Fst::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
